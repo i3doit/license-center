94c434452d85065d53586335050a4bbfc62bfea0
艾兜兜儿 <130721962+i3doit@users.noreply.github.com>
2026-08-19 18:55:31 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-  APP_ID: "",
-  APP_SECRET: "",
+  APP_ID: "cli_aa0f3f274cb89e13",
+  APP_SECRET: "CX1fQ3Qz2nXvnklIdnSwFhZZHYSOsbbB",
 
-  APP_TOKEN: "",
+  APP_TOKEN: "VtS3wpGmMiLXjzkY8SHjtJr6pib",
 
-  LICENSE_TABLE_ID: "",
-  TEMP_TABLE_ID: "",
-  TOOLS_TABLE_ID: ""
+  LICENSE_TABLE_ID: "tblPK4IOMLiIu49K",
+  TEMP_TABLE_ID: "tbl5makPeK4jLeIv",
+  TOOLS_TABLE_ID: "tblnhbRQkNGPRIeG"
 };
